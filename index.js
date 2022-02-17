595b57ee6a54f60554124fb5c6e5c9600f34a714
ramriddlez <ramankangz@outlook.com>
2022-02-17 00:53:16 -0500

    updates in helper js files and more inquirer code in index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
+const { type } = require('os');
 const teamArray = [];
 
 function init() {
    inquirer.prompt([
-    {
+        {
       // role selection
       type: 'list',
       message: 'What is the role for this Employee?',
       name: 'role',
       choices: ["Engineer", "Intern", "Manager"]
-    }])
-    .then(function (data) {
+        }
+    ]).then(function (data) {
         //console.log(data)
         if (data.role === "Engineer") {
             inquirer.prompt([
@@ -28,6 +28,7 @@ function init() {
                 name: "ID",
                 message: "What is your ID Number?",
                 type: "input"
+                //add validate function to validate if number between 1-10
             },
             {
                 name: "github",
@@ -47,17 +48,96 @@ function init() {
                         
                     }
                 }
-            }
-            ]).then((data) => {
-                
-
-            }
-        }});        
-        
-    }
+            }   
+            ]).then(function (data) {
+                //variablizing using the Engineer child class
+                const newEngineer = new Engineer(data.name, data.Id, data.email, data.github);
+                teamArray.push(newEngineer);
+                addUser();
+            })
+            
+        } else if (data.role === "Manager") {
+            inquirer.prompt([
+                {
+                    name:"name",
+                    questions: "What is your name?",
+                    type: "input"
+                },
+                {
+                    name: "ID",
+                    message: "What is your ID Number?",
+                    type: "input"
+                },
+                {
+                    name: "officenumber",
+                    message: "What is your office phone number?",
+                    type: "input"
+                },
+                {
+                    name: "email",
+                    message: "What is your Email?",
+                    type: "input",
+                    validate: function (name) {
+                        let valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(name)
+                        if (valid) {
+                            return true
+                        } else {
+                            return 'Please enter a proper email.'   
+                        }
+                    }
+                }
+                ]).then(function (data) {
+                //variablizing using the Manager child class
+                const newManager = new Manager(data.name, data.Id, data.email, data.officenumber);
+                teamArray.push(newManager);
+                addUser();
+                }) 
+            }    
+        console.log(teamArray)
+            })
+        }
+    
+function addUser(){
+     inquirer.prompt([
+        {   
+            name: "continue",
+            message: "Do you want to add another team member?",
+            type: "confirm"
+        }
+    ])
+        //     ]).then(function(confirmRes){
+        //         confirmRes.continue ? promptUser() : generateHTML()
+        //     })
+        };
 init();
 
-console.log(data)
+
+
+                //function writeToFile(generateMarkdown, data) {
+                    // fs.writeFile('./assets/index.html', newEngineer, (err) => {
+                    //   if (err) {
+                    //     return console.log(err);
+                    //   }
+                    //   console.log("Success!")
+                    // });
+
+
+                //console.log(newEngineer)
+       // })
+        //     }
+        // ).then((EngineerAnswers) => {
+        //         //variablizing using the Engineer child class
+        //         const newEngineer = new Engineer(EngineerAnswers.name, EngineerAnswers.id, EngineerAnswers.email, EngineerAnswers.github);
+
+        //         console.log(newEngineer);
+                
+
+        //     // }
+        // );        
+    
+// init();
+
+//console.log(data)
 
 
 //     {
